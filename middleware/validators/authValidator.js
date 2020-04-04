a297cd63f4c44bbe6147095275ab8c7a724c0390
akshay_balguhare <akshay.balguhare@4thpointer.com>
2020-04-04 13:14:48 +0530

    auth validator modified

--- middleware/validators/authValidator.js
+++ middleware/validators/authValidator.js
@@ -29,18 +29,7 @@ class AuthValidator {
     async userRegisterValidation(req, res, next) {
         const schema = Joi.object().keys({
 
-           
-            
-            role : Joi.string(),
-            state : Joi.string(),
-            country : Joi.string(),
-            phone : Joi.string(),
-            user_id : Joi.string().alphanum().min(5).max(30).required(),
-            dob : Joi.string().required(),
-            name : Joi.string().alphanum().min(3).max(30).required(),
-            password : Joi.string().alphanum().min(3).max(30).required(),
-            email      : Joi.string().email({ minDomainSegments: 2 }).required(),
-            username : Joi.string().alphanum().min(3).max(30).required()
+        
 
         });
 
@@ -52,34 +41,6 @@ class AuthValidator {
         next();
     }
 
-    async warehouseAdminRegisterValidation(req, res, next) {
-        const schema = Joi.object().keys({
-
-            warehouse : Joi.string().required(),
-            location : Joi.string().required(),
-            // department : Joi.string().required(),
-            dob : Joi.string().required(),
-            doj : Joi.string().required(),
-            role : Joi.string().required(),
-            vendor_id : Joi.string(),
-            state : Joi.string(),
-            country : Joi.string(),
-            phone : Joi.string(),
-            status       : Joi.string(),
-            deleted_at : Joi.string(),
-            user_id : Joi.string().alphanum().min(5).max(30),
-            name : Joi.string(),
-            email      : Joi.string().email({ minDomainSegments: 2 }),
-            // username : Joi.string().alphanum().min(3).max(30)
-        });
-
-        const result = await Joi.validate(req.body, schema);
-
-        if(result.error){
-            return Promise.reject(new errors.ValidationFailed());
-        }
-        next();
-    }
 
 }
 
