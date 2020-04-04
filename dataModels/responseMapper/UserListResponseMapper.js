8c9940d5d531c4c191f681b6d8fd158af0d511d6
akshay_balguhare <akshay.balguhare@4thpointer.com>
2020-04-04 11:35:13 +0530

    removed departments

--- dataModels/responseMapper/UserListResponseMapper.js
+++ dataModels/responseMapper/UserListResponseMapper.js
@@ -7,7 +7,6 @@ class UserListResponseMapper {
                 "id": user.id,
                 "name": user.name,
                 "role": user.role,
-                "department": user.department,
                 "user_id": user.user_id,
                 "status": user.status
             };
