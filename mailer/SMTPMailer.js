aaf880618dde9df9945d54e0a7cc25f158cf1f52
Nishikant <nishikant.tyagi@4thpointer.com>
2020-04-10 10:56:39 +0530

    mailho implemented

--- mailer/SMTPMailer.js
+++ mailer/SMTPMailer.js
@@ -5,16 +5,18 @@ class SMTPMailer extends Mailer {
 
     constructor() {
         let serviceName = 'SMTP',
-        transportConf = {
-            service: serviceName,
-            host: config.mailer.host,
-            port: config.mailer.port,
-            secure: config.mailer.secure,
-            auth: {
-                user: config.mailer.email,
-                pass: config.mailer.password
-            }
-        };
+            transportConf = {
+                service: serviceName,
+                host: config.mailer.host,
+                port: config.mailer.port
+            };
+
+        if (config.mailer.secure) {
+            transportConf.secure = config.mailer.secure;
+            transportConf.auth = {};
+            transportConf.auth.user = config.mailer.email;
+            transportConf.auth.password = config.mailer.password;
+        }
 
         super({
             serviceName,
