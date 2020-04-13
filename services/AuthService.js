a8d87fb0bfd824842ef0baf60a30f83b1cf6857f
akshay_balguhare <akshay.balguhare@4thpointer.com>
2020-04-13 20:05:48 +0530

    password already created check

--- services/AuthService.js
+++ services/AuthService.js
@@ -206,8 +206,17 @@ class AuthService {
             await this.validateCreatePassword(tokenData, new_password, confirmPassword);
             let loginDetails = await this.loginUtilityInst.findOne({ user_id: tokenData.user_id })
             if (loginDetails) {
+                if(!loginDetails.is_email_verified)
+                {
+                    return Promise.reject(new errors.ValidationFailed("Email is not verifed"));
+                }
+                if(!loginDetails.forgot_password_token)
+                {
+                    return Promise.reject(new errors.ValidationFailed("Password already created"));
+                }
                 const password = await this.authUtilityInst.bcryptToken(new_password);
-                await this.loginUtilityInst.updateOne({ user_id: loginDetails.user_id }, { is_email_verified: true, status: "active", password: password });
+                await this.loginUtilityInst.updateOne({ user_id: loginDetails.user_id }, {status: "active", password: password,
+                forgot_password_token:""});
                 return Promise.resolve();
             }
             throw new errors.Unauthorized("User is not registered");
