e8a92cae4c6bebe999b173a0c16fc502ab30a722
Nishikant <nishikant.tyagi@4thpointer.com>
2020-04-19 16:13:34 +0000

    docker script modified

--- start_docker.sh
+++ start_docker.sh
@@ -1,2 +1,3 @@
+docker rm -f yftchain-middleware
 docker build -t yftchain-middleware .
-docker run -d --name yftchain-middleware --env-file .env-docker -v /var/www/yftchain-middleware/logs/:/usr/src/app/logs/ -p 3001:3000 --net=host yftchain-middleware 
+docker run -d --name yftchain-middleware --env-file ../.env-docker -v /var/www/yftchain-middleware/logs/:/usr/src/app/logs/ -p 3001:3000 --net=host yftchain-middleware 
