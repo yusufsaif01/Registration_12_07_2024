b86b5ee7c8598475f20694387ce3a56cfc7f546d
akshay_balguhare <akshay.balguhare@4thpointer.com>
2020-04-20 20:28:39 +0530

    indentation in api doc

--- routes/achievement.rest.js
+++ routes/achievement.rest.js
@@ -113,42 +113,42 @@ module.exports = (router) => {
         }));
     });
     /**
-         * @api {post} /achievement/add add achievements
-         * @apiName add achievements
-         * @apiGroup Achievement
-         *   
-         * @apiParam (body) {String} type type of achievement
-         * @apiParam (body) {String} name name of achievement
-         * @apiParam (body) {String} year year of achievement
-         * @apiParam (body) {String} position position achieved
-         * 
-         * @apiSuccess {String} status success
-         * @apiSuccess {String} message Successfully done
-         *
-         * @apiSuccessExample {json} Success-Response:
-         *     HTTP/1.1 200 OK
-         *     {
-         *       "status": "success",
-         *       "message": "Successfully done"
-         *     }   
-         * 
-         * @apiErrorExample {json} Unauthorized
-         *     HTTP/1.1 401 Unauthorized
-         *     {
-         *       "message": "Unauthorized",
-         *       "code": "UNAUTHORIZED",
-         *       "httpCode": 401
-         *     }
-         * 
-         * @apiErrorExample {json} INTERNAL_SERVER_ERROR:
-         *     HTTP/1.1 500 Internal server error
-         *     {
-         *       "message": "Internal Server Error",
-         *       "code": "INTERNAL_SERVER_ERROR",
-         *       "httpCode": 500
-         *     }
-         *
-         */
+ * @api {post} /achievement/add add achievements
+ * @apiName add achievements
+ * @apiGroup Achievement
+ *   
+ * @apiParam (body) {String} type type of achievement
+ * @apiParam (body) {String} name name of achievement
+ * @apiParam (body) {String} year year of achievement
+ * @apiParam (body) {String} position position achieved
+ * 
+ * @apiSuccess {String} status success
+ * @apiSuccess {String} message Successfully done
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "status": "success",
+ *       "message": "Successfully done"
+ *     }   
+ * 
+ * @apiErrorExample {json} Unauthorized
+ *     HTTP/1.1 401 Unauthorized
+ *     {
+ *       "message": "Unauthorized",
+ *       "code": "UNAUTHORIZED",
+ *       "httpCode": 401
+ *     }
+ * 
+ * @apiErrorExample {json} INTERNAL_SERVER_ERROR:
+ *     HTTP/1.1 500 Internal server error
+ *     {
+ *       "message": "Internal Server Error",
+ *       "code": "INTERNAL_SERVER_ERROR",
+ *       "httpCode": 500
+ *     }
+ *
+ */
     router.post('/achievement/add', checkAuthToken, userValidator.addAchievementAPIValidation, async function (req, res) {
         let reqObj = req.body
         try {
