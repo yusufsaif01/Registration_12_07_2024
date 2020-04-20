dea2bdb534ffbcf37321a1ae62bd0f114c072a1f
akshay_balguhare <akshay.balguhare@4thpointer.com>
2020-04-20 12:28:34 +0530

    achievementSchema added

---
+++ db/schemas/AchievementSchema.js
@@ -0,0 +1,46 @@
+const uuidv4 = require("uuid/v4");
+
+module.exports = {
+	fields: {
+		id: {
+			type: String,
+			required: true,
+			unique: true,
+			default: function () {
+				return uuidv4()
+			}
+		},
+		user_id: {
+			type: String,
+			required: true
+        },
+        name: {
+            type: String
+        },
+		type: {
+			type: String,
+			required: true
+		},
+        year: {
+            type: String,
+            required: true
+        },
+        position: {
+            type: String
+        },
+        media_url: {
+            type: String
+        },
+		is_deleted: {
+			type: Boolean,
+			default: false
+		},
+		deleted_at: {
+			type: Date
+		}
+	},
+	schemaName: "achievements",
+	options: {
+		timestamps: true
+	}
+};
