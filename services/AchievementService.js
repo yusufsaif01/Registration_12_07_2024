eeb5d9b0510b6a51ad94b453cb0b4669c74b1383
akshay_balguhare <akshay.balguhare@4thpointer.com>
2020-04-20 16:26:51 +0530

    added achievement list api

--- services/AchievementService.js
+++ services/AchievementService.js
@@ -1,5 +1,7 @@
 const BaseService = require("./BaseService");
+const _ = require("lodash");
 const AchievementUtility = require("../db/utilities/AchievementUtility");
+const AchievementListResponseMapper = require("../dataModels/responseMapper/AchievementListResponseMapper");
 
 class AchievementService extends BaseService {
 
@@ -19,7 +21,32 @@ class AchievementService extends BaseService {
 			}
 			return response;
 		} catch (err) {
+			console.log("Error in stats() of AchievementService", e);
 			return err;
+		}
+	}
+	async getList(requestedData = {}) {
+		try {
+			let response = {}, totalRecords = 0;
+			let paginationOptions = requestedData.paginationOptions || {};
+			let sortOptions = requestedData.sortOptions || {};
+			let skipCount = (paginationOptions.page_no - 1) * paginationOptions.limit;
+			let options = { limit: paginationOptions.limit, skip: skipCount, sort: {} };
+
+			if (!_.isEmpty(sortOptions.sort_by) && !_.isEmpty(sortOptions.sort_order))
+				options.sort[sortOptions.sort_by] = sortOptions.sort_order;
+
+			totalRecords = await this.achievementUtilityInst.countList({ user_id: requestedData.user_id });
+			let data = await this.achievementUtilityInst.find({ user_id: requestedData.user_id }, null, options);
+			data = new AchievementListResponseMapper().map(data);
+			response = {
+				total: totalRecords,
+				records: data
+			}
+			return response;
+		} catch (e) {
+			console.log("Error in getList() of AchievementService", e);
+			return Promise.reject(e);
 		}
 	}
 }
