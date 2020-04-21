d79a3ff36519ee7665db19dc505316165d458a50
akshay_balguhare <akshay.balguhare@4thpointer.com>
2020-04-21 16:58:47 +0530

    modified country service

--- services/CountryService.js
+++ services/CountryService.js
@@ -3,24 +3,16 @@ const CountryUtility = require("../db/utilities/CountryUtility");
 
 class CountryService extends BaseService {
 
-	constructor(){
-		super();
-		this.countryUtilityInst = new CountryUtility();
-	}
-    addIndia(){
+    constructor() {
+        super();
+        this.countryUtilityInst = new CountryUtility();
+    }
+    addIndia() {
         try {
-
-			const _activityInst = new ActivityUtility();
-			let activity = await _activityInst.insertMany([{
-				user_id: user_id,
-				type: type,
-				activity: new Date()
-			}]);
-
-			return activity;
-		} catch (err) {
-			return err;
-		}
+            await this.countryUtilityInst.insert({ name: "India" })
+        } catch (err) {
+            return err;
+        }
     }
 }
 
