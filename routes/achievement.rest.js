92dc4d18c2b8ff9787c096a6e1233e02c7988b66
akshay_balguhare <akshay.balguhare@4thpointer.com>
2020-04-21 16:04:10 +0530

    minor change in upload of add func

--- routes/achievement.rest.js
+++ routes/achievement.rest.js
@@ -155,7 +155,7 @@ module.exports = (router) => {
             if (req.files) {
                 const _fileInst = new FileService();
                 if (req.files.achievement) {
-                    let media_url = await _fileInst.uploadFile(req.files.achievement, "./documents/", req.files.achievement);
+                    let media_url = await _fileInst.uploadFile(req.files.achievement, "./documents/", req.files.achievement.name);
                     reqObj.media_url = media_url;
                 }
             }
