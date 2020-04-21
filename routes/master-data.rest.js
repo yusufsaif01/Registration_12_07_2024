79ffdb74f190a97fff7ece0d5726a96146bfade2
akshay_balguhare <akshay.balguhare@4thpointer.com>
2020-04-22 03:55:56 +0530

    added location stats api doc

--- routes/master-data.rest.js
+++ routes/master-data.rest.js
@@ -318,6 +318,37 @@ module.exports = (router) => {
         let serviceInst = new MasterDataService();
         return responseHandler(req, res, serviceInst.getCountryByCountryCode(req.params.code));
     });
+
+    /**
+    * @api {get} /master/location/stats location stats
+    * @apiName location stats
+    * @apiGroup Location 
+    * 
+    * @apiSuccess {String} status success
+    * @apiSuccess {String} message Successfully done
+    *
+    * @apiSuccessExample {json} Success-Response:
+    *     HTTP/1.1 200 OK
+    *     {
+    *       "status": "success",
+    *       "message": "Successfully done",
+    *       "data": [ {
+    *                  "country": "India",
+    *                  "no_of_state": 10,
+    *                  "no_of_city": 10 }
+    *               ]
+    *     }
+    *
+    * @apiErrorExample {json} INTERNAL_SERVER_ERROR:
+    *     HTTP/1.1 500 Internal server error
+    *     {
+    *       "message": "Internal Server Error",
+    *       "code": "INTERNAL_SERVER_ERROR",
+    *       "httpCode": 500
+    *     }
+    * 
+    */
+
     router.get("/master/location/stats", function (req, res) {
         let serviceInst = new MasterDataService();
         return responseHandler(req, res, serviceInst.getLocationStats());
