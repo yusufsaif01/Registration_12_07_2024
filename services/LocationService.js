05555a965d52ea40ab5a34399026cd0289bdc4c2
akshay_balguhare <akshay.balguhare@4thpointer.com>
2020-04-23 18:36:54 +0530

    removed state and country id from duplicate check

--- services/LocationService.js
+++ services/LocationService.js
@@ -104,10 +104,7 @@ class LocationService {
                 return Promise.reject(new errors.ValidationFailed("name cannot be empty"));
             }
             let regex = new RegExp(["^", reqObj.name, "$"].join(""), "i");
-            const state = await this.stateUtilityInst.findOne({
-                name: regex,
-                country_id: data.country_id, id: data.state_id
-            });
+            const state = await this.stateUtilityInst.findOne({ name: regex });
             if (!_.isEmpty(state)) {
                 return Promise.reject(new errors.Conflict("State already added"));
             }
