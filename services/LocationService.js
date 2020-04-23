05ce48c249c37f1750ff65b602d44632d937c357
akshay_balguhare <akshay.balguhare@4thpointer.com>
2020-04-23 11:46:37 +0530

    CSCClient

--- services/LocationService.js
+++ services/LocationService.js
@@ -1,47 +1,12 @@
-const City = require("csc-client").City;
-const Country = require("csc-client").Country;
-const State = require("csc-client").State;
 const CountryUtility = require('../db/utilities/CountryUtility');
 const _ = require("lodash");
 const LocationListResponseMapper = require("../dataModels/responseMapper/LocationListResponseMapper");
 
 class LocationService {
     constructor() {
-        this.city = new City();
-        this.country = new Country();
-        this.state = new State();
         this.countryUtilityInst = new CountryUtility();
     }
 
-    getAllCountries() {
-        return this.country.getAllCountries();
-    }
-    getCountryById(id) {
-        return this.country.getCountryById(id);
-    }
-    getCountryByCountryCode(code) {
-        return this.country.getCountriesByCountryCode(code);
-    }
-
-    getAllStates() {
-        return this.state.getAllStates();
-    }
-    getStateById(id) {
-        return this.state.getStateById(id);
-    }
-    getStatesByCountryId(countryId) {
-        return this.state.getStatesByCountryId(countryId);
-    }
-
-    getAllCities() {
-        return this.city.getAllCities();
-    }
-    getCityById(id) {
-        return this.city.getCityById(id);
-    }
-    getCitiesByStateId(stateId) {
-        return this.city.getCitiesByStateId(stateId);
-    }
     async getLocationStats() {
         try {
             let data = await this.countryUtilityInst.aggregate([
