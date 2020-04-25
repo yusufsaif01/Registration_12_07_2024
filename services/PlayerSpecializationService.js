01d82c301db2ad6d7746dca99e19051d42924805
akshay_balguhare <akshay.balguhare@4thpointer.com>
2020-04-25 12:35:36 +0530

    changed validation func name

--- services/PlayerSpecializationService.js
+++ services/PlayerSpecializationService.js
@@ -146,7 +146,7 @@ class PlayerSpecializationService {
     async addPosition(data = {}) {
         try {
             let reqObj = data.reqObj;
-            await this.positionValidation(reqObj)
+            await this.addPositionValidation(reqObj)
             let record = {
                 name: reqObj.name,
                 abbreviation: reqObj.abbreviation
@@ -160,7 +160,7 @@ class PlayerSpecializationService {
             return Promise.reject(e);
         }
     }
-    async positionValidation(reqObj = {}) {
+    async addPositionValidation(reqObj = {}) {
         try {
             reqObj.name = reqObj.name.trim().replace(/\s\s+/g, ' ');
             reqObj.abbreviation = reqObj.abbreviation.trim().replace(/\s\s+/g, ' ');
