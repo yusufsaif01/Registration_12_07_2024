44b8498c26160132773256869fecfbf162913369
akshay_balguhare <akshay.balguhare@4thpointer.com>
2020-05-05 15:37:39 +0530

    new password cannot be same as old password functionality

--- constants/ResponseMessage.js
+++ constants/ResponseMessage.js
@@ -62,6 +62,9 @@ class ResponseMessage {
     static get OLD_PASSWORD_INCORRECT() {
         return "Old password is incorrect"
     }
+    static get SAME_PASSWORD() {
+        return "New password is same as old password"
+    }
     static get NEW_PASSWORD_REQUIRED() {
         return "New password is required"
     }
