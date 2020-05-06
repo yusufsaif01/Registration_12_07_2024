24a4107621289e1daa9c86cf98fc385458fff9bc
akshay_balguhare <akshay.balguhare@4thpointer.com>
2020-05-07 01:55:25 +0530

    added projection in followMember func

--- services/ConnectionService.js
+++ services/ConnectionService.js
@@ -10,24 +10,24 @@ class ConnectionService {
 
     async followMember(requestedData = {}) {
         try {
-            let connection_of_sent_by = await this.connectionUtilityInst.findOne({ user_id: requestedData.sent_by });
-            let connection_of_send_to = await this.connectionUtilityInst.findOne({ user_id: requestedData.send_to });
+            let connection_of_sent_by = await this.connectionUtilityInst.findOne({ user_id: requestedData.sent_by }, { followings: 1 });
+            let connection_of_send_to = await this.connectionUtilityInst.findOne({ user_id: requestedData.send_to }, { followers: 1 });
 
             if (!connection_of_sent_by && !connection_of_send_to) {
                 await this.createConnectionAddFollowings(requestedData.sent_by, requestedData.send_to);
                 await this.createConnectionAddFollowers(requestedData.sent_by, requestedData.send_to);
             }
             else if (connection_of_sent_by && !connection_of_send_to) {
-                await this.addFollowings(connection_of_sent_by, requestedData.send_to);
+                await this.addFollowings(connection_of_sent_by, requestedData.sent_by, requestedData.send_to);
                 await this.createConnectionAddFollowers(requestedData.sent_by, requestedData.send_to);
             }
             else if (!connection_of_sent_by && connection_of_send_to) {
                 await this.createConnectionAddFollowings(requestedData.sent_by, requestedData.send_to);
-                await this.addFollowers(requestedData.sent_by, connection_of_send_to);
+                await this.addFollowers(requestedData.sent_by, requestedData.send_to, connection_of_send_to);
             }
             else {
-                await this.addFollowings(connection_of_sent_by, requestedData.send_to);
-                await this.addFollowers(requestedData.sent_by, connection_of_send_to);
+                await this.addFollowings(connection_of_sent_by, requestedData.sent_by, requestedData.send_to);
+                await this.addFollowers(requestedData.sent_by, requestedData.send_to, connection_of_send_to);
             }
             return Promise.resolve();
         }
@@ -47,16 +47,16 @@ class ConnectionService {
         await this.connectionUtilityInst.insert(record);
     }
 
-    async addFollowers(sent_by, connection_of_send_to) {
+    async addFollowers(sent_by, send_to, connection_of_send_to) {
         let followers_of_send_to = connection_of_send_to.followers || [];
         followers_of_send_to.push(sent_by);
-        await this.connectionUtilityInst.updateOne({ user_id: connection_of_send_to.user_id }, { followers: followers_of_send_to });
+        await this.connectionUtilityInst.updateOne({ user_id: send_to }, { followers: followers_of_send_to });
     }
 
-    async addFollowings(connection_of_sent_by, send_to) {
+    async addFollowings(connection_of_sent_by, sent_by, send_to) {
         let followings_of_sent_by = connection_of_sent_by.followings || [];
         followings_of_sent_by.push(send_to);
-        await this.connectionUtilityInst.updateOne({ user_id: connection_of_sent_by.user_id }, { followings: followings_of_sent_by });
+        await this.connectionUtilityInst.updateOne({ user_id: sent_by }, { followings: followings_of_sent_by });
     }
 }
 module.exports = ConnectionService;
