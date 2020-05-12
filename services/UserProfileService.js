023249f20147010b1519b8663c73991488a285b4
akshay_balguhare <akshay.balguhare@4thpointer.com>
2020-05-12 18:13:46 +0530

    added association in profile response

--- services/UserProfileService.js
+++ services/UserProfileService.js
@@ -367,16 +367,16 @@ class UserProfileService {
         institute, documents, about, bio, position, strong_foot, weak_foot, former_club,
         former_academy, specialization, player_type, email, name, avatar_url, state,
         country, city, phone, founded_in, address, stadium_name, owner, manager, short_name,
-        contact_person, trophies, club_academy_details, top_signings, associated_players,
-        registration_number, member_type, social_profiles, type, league, league_other
+        contact_person, trophies, club_academy_details, top_signings, associated_players, registration_number,
+        member_type, social_profiles, type, league, league_other, association, association_other
     }) {
         return {
             nationality, top_players, first_name, last_name, height, weight, dob,
             institute, documents, about, bio, position, strong_foot, weak_foot, former_club,
             former_academy, specialization, player_type, email, name, avatar_url, state,
             country, city, phone, founded_in, address, stadium_name, owner, manager, short_name,
-            contact_person, trophies, club_academy_details, top_signings, associated_players,
-            registration_number, member_type, social_profiles, type, league, league_other
+            contact_person, trophies, club_academy_details, top_signings, associated_players, registration_number,
+            member_type, social_profiles, type, league, league_other, association, association_other
         };
     }
 }
