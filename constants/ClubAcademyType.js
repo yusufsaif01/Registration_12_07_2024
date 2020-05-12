e383604ed9806d2e4d3996cee1a0ea11c00ea158
akshay_balguhare <akshay.balguhare@4thpointer.com>
2020-05-12 11:59:20 +0530

    added type in ClubAcademySchema

---
+++ constants/ClubAcademyType.js
@@ -0,0 +1,10 @@
+class ClubAcademyType {
+    static get RESIDENTIAL() {
+        return "Residential";
+    }
+    static get NON_RESIDENTIAL() {
+        return "Non-Residential";
+    }
+}
+
+module.exports = ClubAcademyType
