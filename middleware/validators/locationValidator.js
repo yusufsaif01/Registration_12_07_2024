abac56eeea68369868778a76f2d17e3c68f5e0b7
akshay_balguhare <akshay.balguhare@4thpointer.com>
2020-05-22 19:18:56 +0530

    updated locationValidator

--- middleware/validators/locationValidator.js
+++ middleware/validators/locationValidator.js
@@ -5,7 +5,11 @@ const responseHandler = require("../../ResponseHandler");
 class LocationValidator {
     async addStateAPIValidation(req, res, next) {
         const schema = Joi.object().keys({
-            "name": Joi.string().required(),
+            "name": Joi.string().required().regex(/^[a-zA-Z0-9\&\- ]+$/).error(() => {
+                return {
+                    message: 'Invalid name',
+                };
+            }),
             "country_id": Joi.string().required()
         });
         try {
@@ -18,7 +22,11 @@ class LocationValidator {
     }
     async editStateAPIValidation(req, res, next) {
         const schema = Joi.object().keys({
-            "name": Joi.string().required()
+            "name": Joi.string().required().regex(/^[a-zA-Z0-9\&\- ]+$/).error(() => {
+                return {
+                    message: 'Invalid name',
+                };
+            })
         });
         try {
             await Joi.validate(req.body, schema);
@@ -30,7 +38,11 @@ class LocationValidator {
     }
     async addCityAPIValidation(req, res, next) {
         const schema = Joi.object().keys({
-            "name": Joi.string().required(),
+            "name": Joi.string().required().regex(/^[a-zA-Z0-9\&\- ]+$/).error(() => {
+                return {
+                    message: 'Invalid name',
+                };
+            }),
             "country_id": Joi.string().required(),
             "state_id": Joi.string().required()
         });
@@ -44,7 +56,11 @@ class LocationValidator {
     }
     async editCityAPIValidation(req, res, next) {
         const schema = Joi.object().keys({
-            "name": Joi.string().required()
+            "name": Joi.string().required().regex(/^[a-zA-Z0-9\&\- ]+$/).error(() => {
+                return {
+                    message: 'Invalid name',
+                };
+            })
         });
         try {
             await Joi.validate(req.body, schema);
