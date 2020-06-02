45e500d0a38bb2db33b587d92377b04b8247b38b
akshay_balguhare <akshay.balguhare@4thpointer.com>
2020-06-02 19:13:29 +0530

    added FootPlayerStatus constant

---
+++ constants/FootPlayerStatus.js
@@ -0,0 +1,13 @@
+class FootPlayerStatus {
+    static get ADDED() {
+        return "added";
+    }
+    static get PENDING() {
+        return "pending";
+    }
+    static get REJECTED() {
+        return "rejected";
+    }
+}
+
+module.exports = FootPlayerStatus
