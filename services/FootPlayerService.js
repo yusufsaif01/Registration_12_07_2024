4ed98dad5cc89015cd463cb4e8322f087f156efc
akshay_balguhare <akshay.balguhare@4thpointer.com>
2020-06-08 13:41:42 +0530

    removed first_name last_name from search conditons

--- services/FootPlayerService.js
+++ services/FootPlayerService.js
@@ -25,7 +25,7 @@ class FootPlayerService {
             let filterConditions = this._preparePlayerFilterCondition(requestedData.filterConditions)
             let data = await this.loginUtilityInst.aggregate([{ $match: { is_deleted: false, member_type: MEMBER.PLAYER } },
             { $project: { user_id: 1, profile_status: 1, _id: 0 } }, { "$lookup": { "from": "player_details", "localField": "user_id", "foreignField": "user_id", "as": "player_detail" } }, { $unwind: { path: "$player_detail" } },
-            { $project: { player_detail: { user_id: 1, email: 1, first_name: 1, last_name: 1, is_verified: { $cond: { if: { $eq: ["$profile_status", PROFILE_STATUS.VERIFIED] }, then: true, else: false } }, position: 1, member_type: MEMBER.PLAYER, player_type: 1, avatar_url: 1, phone: 1 } } },
+            { $project: { player_detail: { user_id: 1, email: 1, first_name: 1, last_name: 1, is_verified: { $cond: { if: { $eq: ["$profile_status.status", PROFILE_STATUS.VERIFIED] }, then: true, else: false } }, position: 1, member_type: MEMBER.PLAYER, player_type: 1, avatar_url: 1, phone: 1 } } },
             { "$lookup": { "from": "foot_players", "localField": "player_detail.user_id", "foreignField": "send_to.user_id", "as": "footplayerDocument" } },
             { $project: { player_detail: 1, filteredfootplayerDocument: { $filter: { input: "$footplayerDocument", as: "element", cond: { $and: [{ $ne: ["$$element.sent_by", requestedData.user_id] }, { $eq: ["$$element.status", FOOTPLAYER_STATUS.ADDED] }, { $eq: ["$$element.is_deleted", false] }] } } } } },
             { $unwind: { path: "$filteredfootplayerDocument", preserveNullAndEmptyArrays: true } },
@@ -38,11 +38,11 @@ class FootPlayerService {
                 filterConditions = this._prepareClubAcademyFilterCondition(requestedData.filterConditions);
                 data = await this.loginUtilityInst.aggregate([{ $match: { is_deleted: false, member_type: { $in: [MEMBER.CLUB, MEMBER.ACADEMY] } } },
                 { $project: { user_id: 1, profile_status: 1, _id: 0 } }, { "$lookup": { "from": "club_academy_details", "localField": "user_id", "foreignField": "user_id", "as": "club_academy_detail" } }, { $unwind: { path: "$club_academy_detail" } },
-                { $project: { club_academy_detail: { user_id: 1, name: 1, member_type: 1, is_verified: { $cond: { if: { $eq: ["$profile_status", PROFILE_STATUS.VERIFIED] }, then: true, else: false } }, type: 1, phone: 1, avatar_url: 1, email: 1 } } },
+                { $project: { club_academy_detail: { user_id: 1, name: 1, member_type: 1, is_verified: { $cond: { if: { $eq: ["$profile_status.status", PROFILE_STATUS.VERIFIED] }, then: true, else: false } }, type: 1, phone: 1, avatar_url: 1, email: 1 } } },
                 { $match: filterConditions }]);
             }
             data = new FootPlayerSearchListResponseMapper().map(data);
-            return { total: data.length, data: data };
+            return { total: data.length, records: data };
         } catch (e) {
             console.log("Error in getPlayerList() of FootPlayerService", e);
             return Promise.reject(e);
@@ -60,12 +60,6 @@ class FootPlayerService {
         let condition = {};
         let filterArr = []
         if (filterConditions) {
-            if (filterConditions.first_name) {
-                filterArr.push({ "player_detail.first_name": new RegExp(filterConditions.first_name, 'i') })
-            }
-            if (filterConditions.last_name) {
-                filterArr.push({ "player_detail.last_name": new RegExp(filterConditions.last_name, 'i') })
-            }
             if (filterConditions.email) {
                 filterArr.push({ "player_detail.email": filterConditions.email })
             }
