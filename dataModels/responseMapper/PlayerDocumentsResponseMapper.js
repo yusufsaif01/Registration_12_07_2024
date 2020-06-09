f6aaea7861e0cd3e8e9ce8d47d16134d1069650c
Aman Kharbanda <aman.kharbanda@4thpointer.com>
2020-06-09 17:25:02 +0530

    fixing listing json structure for - player (yft-724) - club and academy (yft-728)

--- dataModels/responseMapper/PlayerDocumentsResponseMapper.js
+++ dataModels/responseMapper/PlayerDocumentsResponseMapper.js
@@ -3,7 +3,6 @@ class PlayerDocumentsResponseMapper {
     return {
       player_name: [record.first_name, record.last_name].join(" "),
       date_of_birth: record.dob,
-      added_on: record.createdAt,
       documents: record.documents,
     };
   }
