8bd56a879044a45b8f37451b1ab7666861b36d95
akshay_balguhare <akshay.balguhare@4thpointer.com>
2020-06-09 15:31:43 +0530

    manage player response status object fix

--- dataModels/responseMapper/UserListResponseMapper.js
+++ dataModels/responseMapper/UserListResponseMapper.js
@@ -21,8 +21,8 @@ class UserListResponseMapper {
                     data.position = user.position[0].name;
                 }
 
-                if (user.login_details && user.login_details.profile_status) {
-                    data.status = user.login_details.profile_status;
+                if (user.login_details && user.login_details.profile_status && user.login_details.profile_status.status) {
+                    data.status = user.login_details.profile_status.status;
                 }
 
                 if (user.login_details && user.login_details.status) {
