5f70b2cbaaec14ecd04d9d31717fabe9e3522b37
Aman Kharbanda <aman.kharbanda@4thpointer.com>
2020-06-10 12:15:30 +0530

    added more schemas for autoloading

--- db/model/autoload.js
+++ db/model/autoload.js
@@ -4,19 +4,41 @@
 const fs = require("fs");
 
 const modelProvider = require("./index");
-const LoginSchema = require("../schemas/LoginSchema");
 
 /**
  * Add all bootstraping schmeas to load when the
  * mongodb connection is initiated.
  *
  */
-const autoloadModels = [LoginSchema];
+const autoloadModels = [
+    require("../schemas/AbilitySchema"),
+    require("../schemas/AchievementSchema"),
+    require("../schemas/ActivitySchema"),
+    require("../schemas/AdminSchema"),
+    require("../schemas/CitySchema"),
+    require("../schemas/ClubAcademySchema"),
+    require("../schemas/CommentSchema"),
+    require("../schemas/ConnectionRequestSchema"),
+    require("../schemas/ConnectionSchema"),
+    require("../schemas/CountrySchema"),
+    require("../schemas/FootPlayerSchema"),
+    require("../schemas/LikeSchema"),
+    require("../schemas/LoginSchema"),
+    require("../schemas/MemberTypeSchema"),
+    require("../schemas/PlayerSchema"),
+    require("../schemas/PositionSchema"),
+    require("../schemas/PostSchema"),
+    require("../schemas/StateSchema"),
+];
 
 module.exports = () => {
   console.info("[+] Autoloading models...");
   autoloadModels.forEach((modelSchema) => {
-    modelProvider.getModel(modelSchema);
-    console.log("-", modelSchema.schemaName, "Loaded");
+    try {
+      modelProvider.getModel(modelSchema);
+      console.log("-", modelSchema.schemaName, "loaded");
+    } catch (error) {
+      console.error("-", modelSchema.schemaName, "loading failed.");
+    }
   });
 };
