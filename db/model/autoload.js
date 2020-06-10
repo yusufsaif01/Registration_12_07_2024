6bdfd62c2e89fd17fa2880e1e2066b26e8a5cedc
Aman Kharbanda <aman.kharbanda@4thpointer.com>
2020-06-10 12:31:47 +0530

    added option to scan and autoload all models from a directory

--- db/model/autoload.js
+++ db/model/autoload.js
@@ -2,38 +2,53 @@
  * Autolaoding modles on connection.
  */
 const fs = require("fs");
+const path = require("path");
 
 const modelProvider = require("./index");
+
+/**
+ * location to load schmeas from.
+ */
+const schemaLocation = "db/schemas";
 
 /**
  * Add all bootstraping schmeas to load when the
  * mongodb connection is initiated.
  *
  */
-const autoloadModels = [
-    require("../schemas/AbilitySchema"),
-    require("../schemas/AchievementSchema"),
-    require("../schemas/ActivitySchema"),
-    require("../schemas/AdminSchema"),
-    require("../schemas/CitySchema"),
-    require("../schemas/ClubAcademySchema"),
-    require("../schemas/CommentSchema"),
-    require("../schemas/ConnectionRequestSchema"),
-    require("../schemas/ConnectionSchema"),
-    require("../schemas/CountrySchema"),
-    require("../schemas/FootPlayerSchema"),
-    require("../schemas/LikeSchema"),
-    require("../schemas/LoginSchema"),
-    require("../schemas/MemberTypeSchema"),
-    require("../schemas/PlayerSchema"),
-    require("../schemas/PositionSchema"),
-    require("../schemas/PostSchema"),
-    require("../schemas/StateSchema"),
-];
+const autoloadModels = [require("../schemas/LoginSchema")];
 
-module.exports = () => {
+module.exports = (autoloadAll = false) => {
   console.info("[+] Autoloading models...");
-  autoloadModels.forEach((modelSchema) => {
+  if (autoloadAll) {
+    return scanAndAutloadAll();
+  }
+  return loadModels(autoloadModels);
+};
+
+
+/**
+ * Scans directory and autoload models
+ */
+function scanAndAutloadAll() {
+  const schemaDirectory = path.join(__basedir, schemaLocation);
+  console.log("[+] Scanning directory :", schemaDirectory);
+  let schemas = fs.readdirSync(schemaDirectory);
+  console.log("[+] Found", schemas.length, "schmeas");
+
+  loadModels(
+    schemas.map((schema) => {
+      return require(`../schemas/${schema}`);
+    })
+  );
+}
+
+/**
+ * Load models
+ * @param {array} schemaList
+ */
+function loadModels(schemaList) {
+  schemaList.forEach((modelSchema) => {
     try {
       modelProvider.getModel(modelSchema);
       console.log("-", modelSchema.schemaName, "loaded");
@@ -41,4 +56,4 @@ module.exports = () => {
       console.error("-", modelSchema.schemaName, "loading failed.");
     }
   });
-};
+}
