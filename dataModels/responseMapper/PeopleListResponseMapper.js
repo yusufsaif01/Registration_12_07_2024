642c61b8c53241be195649597f4f5bf49ff3dd5d
Aman Kharbanda <aman.kharbanda@4thpointer.com>
2020-06-23 17:31:01 +0530

    added more fields as mentioned in comments

--- dataModels/responseMapper/PeopleListResponseMapper.js
+++ dataModels/responseMapper/PeopleListResponseMapper.js
@@ -1,3 +1,11 @@
+function getAiffNumber(record) {
+  if (record.documentsRequired && record.documentsRequired.length) {
+    return record.documentsRequired[0].document_number;
+  }
+
+  return "";
+}
+
 class PeopleListResponseMapper {
   map(requests) {
     let response = [];
@@ -5,8 +13,11 @@ class PeopleListResponseMapper {
       requests.forEach((request) => {
         let data = {
           user_id: request.user_id,
-          name: request.userDetail[0].name,
-          email: request.userDetail[0].email,
+          name: request.userDetail.name,
+          email: request.userDetail.email,
+          address: request.userDetail.address || "",
+          mobile: request.userDetail.mobile_number || "",
+          aiffNumber: getAiffNumber(request.userDetail),
         };
         response.push(data);
       });
--- services/PeopleService.js
+++ services/PeopleService.js
@@ -4,6 +4,9 @@ const RESPONSE_MESSAGE = require("../constants/ResponseMessage");
 const PlayerUtility = require("../db/utilities/PlayerUtility");
 const LoginUtility = require("../db/utilities/LoginUtility");
 const Role = require("../constants/Role");
+const ProfileStatus = require("../constants/ProfileStatus");
+const AccountStatus = require("../constants/AccountStatus");
+const DocumentType = require("../constants/DocumentType");
 
 const loginInst = new LoginUtility();
 
@@ -19,6 +22,7 @@ class PeopleService {
       aggregatePipes.push(this.addMatchPipeline(params));
       aggregatePipes.push(this.addLookupPipeline());
       aggregatePipes.push(this.addProjectionPipeline());
+      aggregatePipes.push(this.addProjectionPipeline2());
 
       let records = await loginInst.aggregate(aggregatePipes);
 
@@ -33,6 +37,8 @@ class PeopleService {
       $match: {
         is_deleted: false,
         role: params.role || "",
+        status: AccountStatus.ACTIVE,
+        "profile_status.status": ProfileStatus.VERIFIED,
       },
     };
   }
@@ -51,22 +57,39 @@ class PeopleService {
   addProjectionPipeline() {
     return {
       $project: {
+        userDetail: { $arrayElemAt: ["$userDetail", 0] },
+      },
+    };
+  }
+  addProjectionPipeline2() {
+    return {
+      $project: {
         role: 1,
         user_id: 1,
-        "userDetail.name": 1,
-        "userDetail.email": 1,
-        "userDetail.email": 1,
+        userDetail: {
+          name: 1,
+          email: 1,
+          address: "$userDetail.address.full_address",
+          mobile_number:1,
+          documentsRequired: {
+            $filter: {
+              input: "$userDetail.documents",
+              as: "document",
+              cond: { $eq: ["$$document.type", DocumentType.AIFF] },
+            },
+          },
+        },
       },
     };
   }
 
-  validateParams (params) {
+  validateParams(params) {
     if (params.role) {
       if ([Role.CLUB, Role.ACADEMY].indexOf(params.role) == -1) {
-        params.role = ''
+        params.role = "";
       }
     }
-  };
+  }
 }
 
 module.exports = PeopleService;
