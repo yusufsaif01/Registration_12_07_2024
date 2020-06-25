ef84b7301b94da54f1952dd3852fe60e181c79a2
akshay_balguhare <akshay.balguhare@4thpointer.com>
2020-06-25 17:47:51 +0530

    canAddContract field in manage-footplayers list response

--- dataModels/responseMapper/ClubFootPlayersResponseMapping.js
+++ dataModels/responseMapper/ClubFootPlayersResponseMapping.js
@@ -1,3 +1,4 @@
+const FOOTPLAYER_STATUS = require('../../constants/FootPlayerStatus');
 class ClubFootPlayersResponseMapping {
   map(requests, footplayers) {
     let response = [];
@@ -16,8 +17,13 @@ class ClubFootPlayersResponseMapping {
 
         if (footplayers === 0) {
           data.id = request.id;
+          data.canAddContract = false,
           data.email = request.send_to.email;
           data.status = request.status;
+        }
+        if(request.status && request.status === FOOTPLAYER_STATUS.ADDED)
+        {
+          data.canAddContract = request.canAddContract
         }
 
         if (
