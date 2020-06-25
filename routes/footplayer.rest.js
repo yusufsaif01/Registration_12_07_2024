e83a83dbded1db61bfd030fcdeef5c28f28408e7
akshay_balguhare <akshay.balguhare@4thpointer.com>
2020-06-25 18:09:41 +0530

    updated api doc

--- routes/footplayer.rest.js
+++ routes/footplayer.rest.js
@@ -397,6 +397,7 @@ module.exports = (router) => {
     *                      "name": "Rajesh Kumar",
     *                      "position": "Centre Attacking Midfielder",
     *                      "id": "d41d5897-42db-4b0f-aab0-10b08b9b6b09",
+    *                      "canAddContract": true,
     *                      "email": "test@test.com",
     *                      "status": "pending"
     *                  },
