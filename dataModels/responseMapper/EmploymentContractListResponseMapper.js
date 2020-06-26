f6880761f7bbe484a3e16ff856a9022689acb1c8
akshay_balguhare <akshay.balguhare@4thpointer.com>
2020-06-26 18:44:09 +0530

    modified employment-contract-list api for club/academy contracts

---
+++ dataModels/responseMapper/EmploymentContractListResponseMapper.js
@@ -0,0 +1,24 @@
+
+class EmploymentContractListResponseMapper {
+    map(contracts) {
+        let response = [];
+        if (contracts.length) {
+            contracts.forEach((contract) => {
+                let data = {
+                    id: contract.id,
+                    effectiveDate: contract.effectiveDate,
+                    expiryDate: contract.expiryDate,
+                    status: contract.status,
+                    name: contract.name,
+                    clubAcademyUserId: contract.clubAcademyUserId || "",
+                    avatar: contract.avatar || "",
+                    created_by: contract.created_by
+                };
+                response.push(data);
+            });
+        }
+        return response;
+    }
+}
+
+module.exports = EmploymentContractListResponseMapper;
