7d8aea23b80926314030552a9da4c8fd6d0cc6c8
Aman Kharbanda <aman.kharbanda@4thpointer.com>
2020-07-10 13:58:50 +0530

    YFT-975 : Document Approval

--- mailTemplates/documentApprovalTemplate.js
+++ mailTemplates/documentApprovalTemplate.js
@@ -4,5 +4,104 @@ module.exports = ({ email, documentType, name, memberType }) => {
     subject: "Your document details is verified",
     // html: "",
     text: `${documentType} document details for ${name} has been approved successfully by YFTChain.`,
+
+    html(data) {
+      return `
+      <tbody style="display: block;width: 80%; margin:auto;">
+			<tr style="height: 20px;">
+				<td></td>
+			</tr>
+			<!-- Heading of template -->
+			<tr style="display:block; width: 100%;text-align: center;">
+				<td style="display:block; width: 100%; text-align: center;">
+					<h1 style="font-family: 'Paytone One', sans-serif;
+					font-size: 48px;font-weight: 700;color:#626262">
+						Dear ${name},</h1>
+				</td>
+				<td style="display:block; width: 100%; text-align: center;">
+					<h1 style="font-family: 'Paytone One', sans-serif;
+					font-size: 48px;font-weight: 700;color:#626262">
+						Congratulations, you have</h1>
+				</td>
+				<td style="display:block; width: 100%; text-align: center;">
+					<h1 style="font-family: 'Paytone One', sans-serif;
+					font-size: 48px;font-weight: 700;color:#626262">
+						full access now !</h1>
+				</td>
+			</tr>
+			<!--  -->
+
+			<!-- paragraph -->
+			<tr style="height: 20px;">
+				<td></td>
+			</tr>
+			<tr style="display:block; width: 100%;text-align: center;">
+				<td style="display:block; width: 100%; text-align: center;">
+					<p style="font-family: 'Montserrat', sans-serif;
+					font-size: 16px;font-weight: 300;color:#626262;
+					">
+						Thank you for uploading your documents on the YFTChain platform.</p>
+				</td>
+			</tr>
+			<tr style="height: 10px;">
+				<td></td>
+			</tr>
+			<tr style="display:block; width: 100%;text-align: center;">
+				<td style="display:block; width: 100%; text-align: center;">
+					<p style="font-family: 'Montserrat', sans-serif;
+					font-size: 16px;font-weight: 300;color:#626262;
+					">
+						We are pleased to confirm that the verification process for your YFTChain account has been completed according to the data your provided us with !</p>
+				</td>
+			</tr>
+			<tr style="height: 40px;">
+				<td></td>
+			</tr>
+
+			<tr style="display:block; width: 100%;text-align: center;">
+				<td style=" display:block; width: 100%; text-align: center;">
+					<a href="${data.appUrl}member/profile/view" style="background: #FF9933;display: inline-block; border:none; color: #fff;font-family: 'Montserrat',
+						sans-serif;font-size: 18px;border-radius: 10px;text-decoration:none;padding: 15px 40px;">Go to my profile
+					</a>
+				</td>
+			</tr>
+
+			<tr style="height: 40px;">
+				<td></td>
+			</tr>
+
+			<tr style="display:block; width: 100%;text-align: center;">
+				<td style="display:block; width: 100%; text-align: center;">
+					<p style="font-family: 'Montserrat', sans-serif;
+					font-size: 16px;font-weight: 300;color:#626262;
+					">
+						You have successfully unlocked a new world of YFTChain's features. Thank you.</p>
+				</td>
+			</tr>
+			<tr style="height: 20px;">
+				<td></td>
+			</tr>
+
+			<!-- See you online -->
+
+			<tr style="display:block; width: 100%;text-align: center;">
+				<td style=" display:block; width: 100%; text-align: center;">
+					<p style="font-family: 'Montserrat', sans-serif;
+					font-size: 18px;font-weight: 300;color:#626262;
+					">
+						See you online!
+					</p>
+					<p style="font-family: 'Montserrat', sans-serif;
+					font-size: 20px; font-weight: 700;display:block;color:#626262;
+					">YFTChain Team</p>
+				</td>
+			</tr>
+			<tr style=" height: 20px;">
+				<td></td>
+			</tr>
+			<!-- end  -->
+		</tbody>
+      `;
+    }
   };
 };
