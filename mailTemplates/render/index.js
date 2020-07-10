cbdb886dca6934dff6b792728214be4dd6e58ceb
Aman Kharbanda <aman.kharbanda@4thpointer.com>
2020-07-10 11:59:44 +0530

    integrated template in application

--- mailTemplates/render/index.js
+++ mailTemplates/render/index.js
@@ -5,6 +5,17 @@ const config = require("../../config");
 const globalData = {
   appName: config.app.name,
   appUrl: config.app.baseURL,
+  socialLinks: {
+    facebook: config.app.facebook_url,
+    instagram: config.app.instagram_url,
+    linkedin: config.app.linkedin_url,
+    twitter: config.app.twitter_url,
+  },
+  footerLinks: {
+    privacyPolicy: config.app.privacy_policy_url,
+    termsConditions: config.app.terms_and_conditions_url,
+    contactUs: config.app.contact_us_url,
+  },
 };
 
 module.exports = (html, data = {}) => {
--- mailTemplates/render/partials/footer.js
+++ mailTemplates/render/partials/footer.js
@@ -1,5 +1,136 @@
 module.exports = (footer) => {
   return `
-        <h1>Footer Tag</h1>
+      <tfoot
+        style="
+          display: block;
+          width: 100%;
+          background: #fbf9f9;
+          border-bottom-left-radius: 10px;
+          border-bottom-right-radius: 10px;
+        "
+      >
+        <tr style="height: 20px;">
+          <td></td>
+        </tr>
+        <!-- social media -->
+        <tr style="display: block; width: 100%;">
+          <td style="display: block; width: 100%; text-align: center;">
+            <!-- social -->
+            <div style="display: block; width: 100%; text-align: center;">
+              <ul style="list-style: none; padding: 0px;">
+                <li style="display: inline-block; width: 50px;">
+                  <a
+                    href="${footer.socialLinks.instagram}"
+                    style="display: inline-block; text-decoration: none;"
+                  >
+                    <img src="${footer.appUrl}assets/images/instagram.png" alt="instagram" />
+                  </a>
+                </li>
+                <li style="display: inline-block; width: 50px;">
+                  <a
+                    href="${footer.socialLinks.facebook}"
+                    style="display: inline-block; text-decoration: none;"
+                  >
+                    <img src="${footer.appUrl}assets/images/facebook.png" alt="facebook" />
+                  </a>
+                </li>
+                <li style="display: inline-block; width: 50px;">
+                  <a
+                    href="${footer.socialLinks.linkedin}"
+                    style="display: inline-block; text-decoration: none;"
+                  >
+                    <img src="${footer.appUrl}assets/images/linkedin.png" alt="Linkedin" />
+                  </a>
+                </li>
+                <li style="display: inline-block; width: 50px;">
+                  <a
+                    href="${footer.socialLinks.twitter}"
+                    style="display: inline-block; text-decoration: none;"
+                  >
+                    <img src="${footer.appUrl}assets/images/twitter.png" alt="twitter" />
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </td>
+        </tr>
+        <tr style="height: 10px;">
+          <td></td>
+        </tr>
+        <!-- copyright text -->
+        <tr style="display: block; width: 100%; height: 30px;">
+          <td style="display: block; width: 100%; text-align: center;">
+            <p
+              style="
+                font-family: 'Montserrat', sans-serif;
+                font-size: 16px;
+                font-weight: 600;
+                color: #626262;
+              "
+            >
+              Copyright @ 2020, ${footer.appName}, All rights are reserved.
+            </p>
+          </td>
+        </tr>
+        <!-- footerlinks -->
+        <tr style="display: block; width: 100%; height: 30px;">
+          <td style="display: block; width: 100%; text-align: center;">
+            <ul>
+              <li style="display: inline-block; width: 120px;">
+                <a
+                  style="
+                    font-family: 'Montserrat', sans-serif;
+                    text-decoration: none;
+                    font-size: 16px;
+                    font-weight: 600;
+                    color: #ff9933;
+                  "
+                  href="${footer.footerLinks.privacyPolicy}"
+                  >Privacy policy</a
+                >
+              </li>
+              <li style="display: inline-block; width: 10px; font-weight: 700;">
+                |
+              </li>
+              <li style="display: inline-block; width: 180px;">
+                <a
+                  style="
+                    font-family: 'Montserrat', sans-serif;
+                    text-decoration: none;
+                    font-size: 16px;
+                    font-weight: 600;
+                    color: #ff9933;
+                  "
+                  href="${footer.footerLinks.termsConditions}"
+                  >Terms & Conditions</a
+                >
+              </li>
+              <li style="display: inline-block; width: 10px; font-weight: 700;">
+                |
+              </li>
+              <li style="display: inline-block; width: 120px;">
+                <a
+                  style="
+                    font-family: 'Montserrat', sans-serif;
+                    text-decoration: none;
+                    font-size: 16px;
+                    font-weight: 600;
+                    color: #ff9933;
+                  "
+                  href="${footer.footerLinks.contactUs}"
+                  >Contact us</a
+                >
+              </li>
+            </ul>
+          </td>
+        </tr>
+        <tr style="height: 20px;">
+          <td></td>
+        </tr>
+      </tfoot>
+    </table>
+  </body>
+</html>
+
     `;
 };
