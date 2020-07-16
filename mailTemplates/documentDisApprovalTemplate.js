959c448df803f0b66b53179434282dae66b6e698
Aman Kharbanda <aman.kharbanda@4thpointer.com>
2020-07-16 14:44:28 +0530

    YFT-1103 : Document disapproval template for member.

--- mailTemplates/documentDisApprovalTemplate.js
+++ mailTemplates/documentDisApprovalTemplate.js
@@ -4,5 +4,109 @@ module.exports = ({ email, documentType, name, memberType, reason }) => {
     subject: "Your document details is disapproved",
     // html: "",
     text: `${documentType} document details for ${name} has been disapproved by YFTChain due to "${reason}", please update again.`,
+
+    html(data) {
+      return `
+      <tbody style="display: block;width: 80%; margin:auto;">
+			<tr style="height: 20px;">
+				<td></td>
+			</tr>
+			<!-- Heading of template -->
+			<tr style="display:block; width: 100%;text-align: center;">
+				<td style="display:block; width: 100%; text-align: center;">
+					<h1 style="font-family: 'Paytone One', sans-serif;
+					font-size: 48px;font-weight: 700;color:#626262">
+						Dear ${name},</h1>
+				</td>
+				<td style="display:block; width: 100%; text-align: center;">
+					<h1 style="font-family: 'Paytone One', sans-serif;
+					font-size: 48px;font-weight: 700;color:#626262">
+						Sorry, looks like your document</h1>
+				</td>
+				<td style="display:block; width: 100%; text-align: center;">
+					<h1 style="font-family: 'Paytone One', sans-serif;
+					font-size: 48px;font-weight: 700;color:#626262">
+						needs to be updated</h1>
+				</td>
+			</tr>
+			<!--  -->
+
+			<!-- paragraph -->
+			<tr style="height: 20px;">
+				<td></td>
+			</tr>
+			<tr style="display:block; width: 100%;text-align: center;">
+				<td style="display:block; width: 100%; text-align: center;">
+					<p style="font-family: 'Montserrat', sans-serif;
+					font-size: 16px;font-weight: 300;color:#626262;
+					">
+						We apologies to inform you that YFTChain hs not approved your <br> document information due to ${reason} </p>
+				</td>
+			</tr>
+			<tr style="height: 10px;">
+				<td></td>
+			</tr>
+			<tr style="display:block; width: 100%;text-align: center;">
+				<td style="display:block; width: 100%; text-align: center;">
+					<p style="font-family: 'Montserrat', sans-serif;
+					font-size: 16px;font-weight: 300;color:#626262;
+					">
+            We request that you as a player verify that you re holding a valid document by
+            <br> providing correct information.
+            <br> Please click below to go the YFTChain's portal and add/update your document information  
+          </p>
+				</td>
+			</tr>
+			<tr style="height: 40px;">
+				<td></td>
+			</tr>
+
+			<tr style="display:block; width: 100%;text-align: center;">
+				<td style=" display:block; width: 100%; text-align: center;">
+					<a href="${data.appUrl}member/profile/view" style="background: #FF9933;display: inline-block; border:none; color: #fff;font-family: 'Montserrat',
+						sans-serif;font-size: 18px;border-radius: 10px;text-decoration:none;padding: 15px 40px;">Go to my profile
+					</a>
+				</td>
+			</tr>
+
+			<tr style="height: 40px;">
+				<td></td>
+			</tr>
+
+			<tr style="display:block; width: 100%;text-align: center;">
+				<td style="display:block; width: 100%; text-align: center;">
+					<p style="font-family: 'Montserrat', sans-serif;
+					font-size: 16px;font-weight: 300;color:#626262;
+					">
+            Once your document has been approved, you will be treated as a verified 
+            <br> player on the YFTChain portal.
+          </p>
+				</td>
+			</tr>
+			<tr style="height: 20px;">
+				<td></td>
+			</tr>
+
+			<!-- See you online -->
+
+			<tr style="display:block; width: 100%;text-align: center;">
+				<td style=" display:block; width: 100%; text-align: center;">
+					<p style="font-family: 'Montserrat', sans-serif;
+					font-size: 18px;font-weight: 300;color:#626262;
+					">
+						See you online!
+					</p>
+					<p style="font-family: 'Montserrat', sans-serif;
+					font-size: 20px; font-weight: 700;display:block;color:#626262;
+					">YFTChain Team</p>
+				</td>
+			</tr>
+			<tr style=" height: 20px;">
+				<td></td>
+			</tr>
+			<!-- end  -->
+		</tbody>
+      `;
+    },
   };
 };
