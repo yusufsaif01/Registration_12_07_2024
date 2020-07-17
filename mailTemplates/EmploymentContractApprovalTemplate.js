7f8fe1cd5f4eef11c6366ff651a9e4f35d06eb0d
Aman Kharbanda <aman.kharbanda@4thpointer.com>
2020-07-17 13:13:57 +0530

    removed unused email template

--- mailTemplates/EmploymentContractApprovalTemplate.js
+++
@@ -1,8 +0,0 @@
-module.exports = ({ email, name }) => {
-    return {
-      to: email,
-      subject: "Contract Approved",
-      // html: "",
-      text: `Employment Contract for ${name} has been approved successfully.`,
-    };
-  };
