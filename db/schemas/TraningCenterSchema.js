44da27cb056e777de888530278f9bc09ba2f00ed
yusufsaif0 <yusufsaif0@gmail.com>
2024-06-26 23:12:13 +0530

    middlewre

--- db/schemas/TraningCenterSchema.js
+++ db/schemas/TraningCenterSchema.js
@@ -42,10 +42,19 @@ module.exports = {
     coache_name: {
       type: String,
     },
-
-    opening_days: {
-      type: String
-    },
+    opening_days: [
+      {
+        item_id: {
+          type: Number,
+        },
+        item_text: {
+          type: String,
+        },
+      },
+    ],
+    //opening_days: {
+    //  type: String
+    //},
     is_deleted: {
       type: Boolean,
       default: false,
